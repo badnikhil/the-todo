006462b9e47d677216fd046979817091bef1b67d
badnikhil <nikhilljatt@gmail.com>
2026-06-05 13:42:28 +0530

    update schemas

--- backend/models.py
+++ backend/models.py
@@ -6,5 +6,5 @@ class Todo(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
-    description = Column(String, index=True)
+    description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
